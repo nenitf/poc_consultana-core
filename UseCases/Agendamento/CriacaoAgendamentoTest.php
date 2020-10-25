57259559250c5cd50933625d8e6debafddb989b8
Felipe Silva <felipe.dacs@gmail.com>
2020-10-25 08:07:48 -0300

    :cop: refatora com abstração de given/when/then

--- UseCases/Agendamento/CriacaoAgendamentoTest.php
+++ UseCases/Agendamento/CriacaoAgendamentoTest.php
@@ -79,6 +79,19 @@ class CriacaoAgendamentoTest extends \PHPUnit\Framework\TestCase
     private function given($contexto, ...$params)
     {
         switch($contexto) {
+        case "médico encontrado: desativado":
+            $medico = $this->newMedico(
+                123456,
+                "Robson",
+                [
+                    $this->newHorarioDisponivel("8:00", "18:00"),
+                    $this->newHorarioDisponivel("09:00", "14:00")
+                ],
+                true
+            );
+            return $this->given("médico encontrado", $medico);
+        case "médico não encontrado":
+            return $this->given("médico encontrado", null);
         case "médico encontrado":
             $this->doubleMedicosRepository
                  ->expects($this->once())
@@ -86,19 +99,49 @@ class CriacaoAgendamentoTest extends \PHPUnit\Framework\TestCase
                  ->willReturn($params[0]);
             break;
         case "horário disponível":
-        $this->doubleAgendamentosRepository
-             ->expects($this->once())
-             ->method('findByIntervalo')
-             ->with(
-                 $this->callback(function($h) use ($params){
-                     return $h->format('Y-m-d H:i') === $params[0];
-                 }),
-                 $this->callback(function($h) use ($params){
-                     return $h->format('Y-m-d H:i') === $params[1];
-                 })
-             )
-             ->willReturn(null);
+            $this->doubleAgendamentosRepository
+                 ->expects($this->once())
+                 ->method('findByIntervalo')
+                 ->with(
+                     $this->callback(function($h) use ($params){
+                         return $h->format('Y-m-d H:i') === $params[0];
+                     }),
+                     $this->callback(function($h) use ($params){
+                         return $h->format('Y-m-d H:i') === $params[1];
+                     })
+                 )
+                 ->willReturn(null);
             break;
+        default:
+            throw new \Exception("Nenhum contexto de \"given\" foi encontrado");
+        }
+        return $this;
+    }
+
+    private function when($contexto, ...$params)
+    {
+        switch($contexto){
+        case "médico 51, paciente 54, em 2020-10-06T09:30 com 30":
+            return $this->newDTO(
+                51, 54, new \DateTime('2020-10-06T09:30'), 30
+            );
+            break;
+        default:
+            throw new \Exception("Nenhum contexto de \"when\" foi encontrado");
+        }
+
+        return $this;
+    }
+
+    private function then($contexto, ...$params)
+    {
+        switch($contexto){
+        case "ocorre erro":
+            $this->expectException(AppException::class);
+            $this->expectExceptionMessage($params[0]);
+            break;
+        default:
+            throw new \Exception("Nenhum contexto de \"then\" foi encontrado");
         }
         return $this;
     }
@@ -127,29 +170,41 @@ class CriacaoAgendamentoTest extends \PHPUnit\Framework\TestCase
         $sut->execute($dto);
     }
 
-    public function testNaoDeveAgendarMedicoDesativado()
-    {
-        $medico = $this->newMedico(
-            123456,
-            "Robson",
-            [
-                $this->newHorarioDisponivel("8:00", "18:00"),
-                $this->newHorarioDisponivel("09:00", "14:00")
-            ],
-            true
-        );
-
-        $this->given("médico encontrado", $medico);
+    /**
+     * @dataProvider providerAppExceptions
+     * @testdox $testName
+     */
+    public function testNaoDeveCriarAgendamento(
+        string $testName, array $givens, $when, $errorMessage
+    ) {
+        foreach($givens as $given){
+            $this->given($given);
+        }
+        $dto = $this->when($when);
+        $this->then("ocorre erro", $errorMessage);
 
         $sut = $this->newSut();
-
-        $dto = $this->newDTO(
-            $medico->getId(), 54, new \DateTime('2020-10-06T09:30'), 30
-        );
-
-        $this->expectException(AppException::class);
-        $this->expectExceptionMessage("Não é possível consultar com médico desativado");
-
         $sut->execute($dto);
+    }
+
+    public function providerAppExceptions()
+    {
+        yield [
+            "Não deve agendar com medico desativado",
+            [
+                "médico encontrado: desativado"
+            ],
+            "médico 51, paciente 54, em 2020-10-06T09:30 com 30",
+            "Não é possível consultar com médico desativado",
+        ];
+
+        yield [
+            "Não deve agendar sem médico",
+            [
+                "médico não encontrado"
+            ],
+            "médico 51, paciente 54, em 2020-10-06T09:30 com 30",
+            "Médico não encontrado",
+        ];
     }
 }
