29088acf82e7385d03333091c85ea989014067fd
Felipe Silva <felipe.dacs@gmail.com>
2020-10-25 07:25:01 -0300

    :cop: add teste desativado e add given

--- UseCases/Agendamento/CriacaoAgendamentoTest.php
+++ UseCases/Agendamento/CriacaoAgendamentoTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use Core\Exceptions\AppException;
+
 use Core\UseCases\Agendamento\{
     CriacaoAgendamento,
     CriacaoAgendamentoDTO,
@@ -52,11 +54,12 @@ class CriacaoAgendamentoTest extends \PHPUnit\Framework\TestCase
                         ->setFim($fim);
     }
 
-    private function newMedico($id, $nome, $horariosDisponiveis)
+    private function newMedico($id, $nome, $horariosDisponiveis, $desativado)
     {
         $medico = new Medico();
         $medico->setId($id)
-               ->setNome($nome);
+               ->setNome($nome)
+               ->setDesativado($desativado);
 
         if(is_array($horariosDisponiveis)){
             $medico->setHorariosDisponiveis(...$horariosDisponiveis);
@@ -73,6 +76,33 @@ class CriacaoAgendamentoTest extends \PHPUnit\Framework\TestCase
                    ->setDuracao($duracao);
     }
 
+    private function given($contexto, ...$params)
+    {
+        switch($contexto) {
+        case "médico encontrado":
+            $this->doubleMedicosRepository
+                 ->expects($this->once())
+                 ->method('findById')
+                 ->willReturn($params[0]);
+            break;
+        case "horário disponível":
+        $this->doubleAgendamentosRepository
+             ->expects($this->once())
+             ->method('findByIntervalo')
+             ->with(
+                 $this->callback(function($h) use ($params){
+                     return $h->format('Y-m-d H:i') === $params[0];
+                 }),
+                 $this->callback(function($h) use ($params){
+                     return $h->format('Y-m-d H:i') === $params[1];
+                 })
+             )
+             ->willReturn(null);
+            break;
+        }
+        return $this;
+    }
+
     public function testDeveAgendarConsulta()
     {
         $medico = $this->newMedico(
@@ -81,32 +111,44 @@ class CriacaoAgendamentoTest extends \PHPUnit\Framework\TestCase
             [
                 $this->newHorarioDisponivel("8:00", "18:00"),
                 $this->newHorarioDisponivel("09:00", "14:00")
-            ]
+            ],
+            false
         );
 
-        $this->doubleMedicosRepository
-             ->expects($this->once())
-             ->method('findById')
-             ->willReturn($medico);
-
-        $this->doubleAgendamentosRepository
-             ->expects($this->once())
-             ->method('findByIntervalo')
-             ->with(
-                 $this->callback(function($h){
-                     return $h->format('Y-m-d H:i') === '2020-10-06 09:30';
-                 }),
-                 $this->callback(function($h){
-                     return $h->format('Y-m-d H:i') === '2020-10-06 10:00';
-                 })
-             )
-             ->willReturn(null);
+        $this->given("médico encontrado", $medico)
+             ->given("horário disponível", '2020-10-06 09:30', '2020-10-06 10:00');
 
         $sut = $this->newSut();
 
         $dto = $this->newDTO(
             $medico->getId(), 54, new \DateTime('2020-10-06T09:30'), 30
         );
+
+        $sut->execute($dto);
+    }
+
+    public function testNaoDeveAgendarMedicoDesativado()
+    {
+        $medico = $this->newMedico(
+            123456,
+            "Robson",
+            [
+                $this->newHorarioDisponivel("8:00", "18:00"),
+                $this->newHorarioDisponivel("09:00", "14:00")
+            ],
+            true
+        );
+
+        $this->given("médico encontrado", $medico);
+
+        $sut = $this->newSut();
+
+        $dto = $this->newDTO(
+            $medico->getId(), 54, new \DateTime('2020-10-06T09:30'), 30
+        );
+
+        $this->expectException(AppException::class);
+        $this->expectExceptionMessage("Não é possível consultar com médico desativado");
 
         $sut->execute($dto);
     }
